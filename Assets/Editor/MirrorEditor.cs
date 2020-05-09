c33d02623f11863ce67f6bcd28afe2dfe8ad9f1e
davidsheh <davidsheh1015@gmail.com>
2020-05-09 19:42:36 +0800

    重新替换新的 ObjectPool

---
+++ Assets/Editor/MirrorEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UI.Effect;
+
+namespace UIEditor.Effect
+{
+    [CustomEditor(typeof(Mirror), true)]
+    [CanEditMultipleObjects]
+    public class MirrorEditor : Editor
+    {
+        protected SerializedProperty m_MirrorType;
+
+        private GUIContent m_CorrectButtonContent;
+        private GUIContent m_MirrorTypeContent;
+
+        protected virtual void OnDisable()
+        {
+            
+        }
+
+        protected virtual void OnEnable()
+        {
+            m_CorrectButtonContent = new GUIContent("Set Native Size", "Sets the size to match the content.");
+
+            m_MirrorTypeContent = new GUIContent("Mirror Type");
+
+            m_MirrorType = serializedObject.FindProperty("m_MirrorType");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(m_MirrorType, m_MirrorTypeContent);
+
+            if (GUILayout.Button(m_CorrectButtonContent, EditorStyles.miniButton))
+            {
+                int len = targets.Length;
+
+                for (int i = 0; i < len; i++)
+                {
+                    if (targets[i] is Mirror)
+                    {
+                        Mirror mirror = targets[i] as Mirror;
+
+                        Undo.RecordObject(mirror.rectTransform, "Set Native Size");
+                        mirror.SetNativeSize();
+                        EditorUtility.SetDirty(mirror);
+                    }
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/ObjectPool/ListPool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ObjectPool
+{
+    internal static class ListPool<T>
+    {
+        private static readonly Pool<List<T>> _listPool = new Pool<List<T>>(null, l => l.Clear());
+
+        public static List<T> Get()
+        {
+            return _listPool.Get();
+        }
+
+        public static void Recycle(List<T> element)
+        {
+            _listPool.Recycle(element);
+        }
+    }
+}
