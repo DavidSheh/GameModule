3ac5ed761fb726ba7ff6bb0511e7c88a6fc22605
davidsheh <davidsheh1015@gmail.com>
2026-07-17 14:49:21 +0800

    添加一个新的对象池实现

---
+++ Assets/Object Pool 2/PoolObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace OWS.ObjectPooling
+{
+    public class PoolObject : MonoBehaviour, IPoolable<PoolObject>
+    {
+        private Action<PoolObject> returnToPool;
+
+        private void OnDisable()
+        {
+            ReturnToPool();
+        }
+
+        public void Initialize(Action<PoolObject> returnAction)
+        {
+            //cache reference to return action
+            this.returnToPool = returnAction;
+        }
+
+        public void ReturnToPool()
+        {
+            //invoke and return this object to pool
+            returnToPool?.Invoke(this);
+        }
+    }
+}
+
